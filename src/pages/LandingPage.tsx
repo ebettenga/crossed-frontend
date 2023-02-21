5a82879cd844c9136a302b2a5ee4890c39d5b18d
Ethan Bettenga <ethan@listenersoncall.com>
2023-02-20 22:37:56 -0600

    authentication working

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,20 +1,35 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 export const LandingPage = () => {
-  const { user, loginWithPopup } = useAuth0();
+  const { user, loginWithPopup, getAccessTokenSilently } = useAuth0();
 
   const login = () => {
     loginWithPopup({
-      authorizationParams: { audience: import.meta.env.VITE_APP_AUTH0_AUDIENCE, redirect_uri: import.meta.env.VITE_APP_AUTH0_CALLBACK_URL },
+      authorizationParams: {
+        audience: import.meta.env.VITE_APP_AUTH0_AUDIENCE,
+        redirect_uri: import.meta.env.VITE_APP_AUTH0_CALLBACK_URL,
+      },
     }).then((data) => {
+      
       console.log(data);
     });
   };
 
+  const makeRequest = async () => {
+    let token = "";
+    await getAccessTokenSilently().then((data) => {
+     token = data 
+    })
+    fetch("http://localhost:5000/api/v1/crosswords?page=1&limit=4&dow=Tuesday", {headers: [['Authorization', `Bearer ${token}`]]}).then(data => {
+      console.log(data)
+    })
+  }
+
   return (
     <div>
-      <div>{user?.given_name}</div>
+      <div>{user?.email}</div>
       <button onClick={login}>Login</button>
+      <button onClick={() => makeRequest()}>Make request</button>
     </div>
   );
 };
