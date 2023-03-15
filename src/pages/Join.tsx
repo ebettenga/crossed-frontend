91fa361498d997d47bca45022444d697e385bcf5
Ethan Bettenga <ethan@listenersoncall.com>
2023-03-15 17:54:49 -0500

    basic css for Join Page

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -3,8 +3,43 @@ import { PageState } from "../App";
 import { useCurrentUser } from "../domains/user/use-current-user";
 import { JoinRoomPayload, SessionData } from "../domains/socket/use-web-socket";
 import invariant from "tiny-invariant";
-import { PageContainer, Spacer } from "../domains/components/spacing";
-import { Button } from "antd";
+import { Button, Layout, Space } from "antd";
+import Title from "antd/es/typography/Title";
+
+const { Content } = Layout;
+
+const JoinPageContainer: React.FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <Layout
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <JoinRoomTitle />
+      <div style={{ paddingTop: "30px" }}></div>
+      <Content
+        style={{
+          maxWidth: "500px",
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          alignItems: "space-between",
+        }}
+      >
+        {children}
+      </Content>
+    </Layout>
+  );
+};
+
+export interface ButtonInfo {
+  difficulty: "easy" | "medium" | "hard";
+  title: "Easy" | "Medium" | "Hard";
+}
 
 export const JoinRoom = ({
   sessionData,
@@ -21,60 +56,57 @@ export const JoinRoom = ({
     }
   }, [sessionData?.roomId]);
 
+  const buttonData: ButtonInfo[] = [
+    {
+      difficulty: "easy",
+      title: "Easy",
+    },
+    {
+      difficulty: "medium",
+      title: "Medium",
+    },
+    {
+      difficulty: "hard",
+      title: "Hard",
+    },
+  ];
+
   return (
-    <PageContainer>
-      <JoinRoomTitle />
-      <Spacer margin={"10vh"} />
-      <DifficultlyButtonContainer>
-        <SelectDifficultyButton
-          difficulty="easy"
-          joinRoom={joinRoom}
-          title={"Easy"}
-        />
-        <SelectDifficultyButton
-          difficulty="medium"
-          joinRoom={joinRoom}
-          title={"Medium"}
-        />
-        <SelectDifficultyButton
-          difficulty="hard"
-          joinRoom={joinRoom}
-          title={"Hard"}
-        />
-      </DifficultlyButtonContainer>
-    </PageContainer>
+    <JoinPageContainer>
+      <Space>
+        {buttonData.map(({ difficulty, title }) => {
+          return (
+            <>
+              <SelectDifficultyButton
+                difficulty={difficulty}
+                joinRoom={joinRoom}
+                title={title}
+              />
+            </>
+          );
+        })}
+      </Space>
+    </JoinPageContainer>
   );
 };
 
-const JoinRoomTitle = () => <h1>Select Difficulty</h1>;
-
-const DifficultlyButtonContainer: React.FC<{ children: ReactNode }> = ({
-  children,
-}) => (
-  <div
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-    }}
-  >
-    {children}
-  </div>
-);
+const JoinRoomTitle = () => <Title>Select Difficulty</Title>;
 
 const SelectDifficultyButton: React.FC<{
   joinRoom: (joinRoomPayload: JoinRoomPayload) => void;
   difficulty: "easy" | "medium" | "hard";
   title: "Easy" | "Medium" | "Hard";
 }> = ({ joinRoom, difficulty, title }) => {
-  const {user} = useCurrentUser();
+  const { user } = useCurrentUser();
   invariant(user, "user not found during difficulty selection");
   return (
     <>
-      <Button onClick={() => joinRoom({ difficulty, user_id: user.id })}>
+      <Button
+        type="primary"
+        onClick={() => joinRoom({ difficulty, user_id: user.id })}
+      >
         {title}
       </Button>
-      <Spacer margin={"5vh"} />
     </>
   );
 };
